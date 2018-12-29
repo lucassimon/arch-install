c71c8061c8d3702ba891817e31856f488d79f26b
Lucas Simon Rodrigues Magalhaes <lucassrod@gmail.com>
2018-12-29 21:08:19 -0200

    Update 99-devtools.sh

--- 99-devtools.sh
+++ 99-devtools.sh
@@ -3,6 +3,10 @@ set -e
 
 echo "sublime"
 # https://www.sublimetext.com/docs/3/linux_repositories.html
+# curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+# echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+# echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/dev/x86_64" | sudo tee -a /etc/pacman.conf
+
 yay -S --noconfirm sublime-text-dev
 
 echo "vscode"
