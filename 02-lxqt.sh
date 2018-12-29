3e031e6b5e90497c8896f8b13bb3a1b768adfb41
Lucas Simon Rodrigues Magalhaes <lucassrod@gmail.com>
2018-12-29 21:04:00 -0200

    Update 02-lxqt.sh

--- 02-lxqt.sh
+++ 02-lxqt.sh
@@ -33,7 +33,7 @@ sudo pacman -S gst-plugins-good gst-plugins-bad gst-plugins-base gst-plugins-ugl
 
 echo "https://github.com/gabretana/lxqt-arc-dark-theme"
 
-sudo pacman -S arc-kde kvantum-theme-arc papirus-icon-theme --noconfirm
+sudo pacman -S arc-kde kvantum-theme-arc adapta-kde kvantum-theme-adapta papirus-icon-theme --noconfirm
 
 
 
